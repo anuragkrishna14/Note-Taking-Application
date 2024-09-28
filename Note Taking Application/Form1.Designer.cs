1e6d0f67b5b3894d462ddeb4cb5e54a306fd432a
Anurag Krishna <anuragkrishna14@gmail.com>
2024-09-28 10:34:34 +0530

    Complete the Project and add .gitignore file

---
+++ Note Taking Application/Form1.Designer.cs
@@ -0,0 +1,260 @@
+﻿
+namespace Note_Taking_Application
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnRead = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnNew = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.lbNotes = new System.Windows.Forms.ListBox();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.txtTitle = new System.Windows.Forms.TextBox();
+            this.txtNote = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnRead
+            // 
+            this.btnRead.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.btnRead.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnRead.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnRead.Font = new System.Drawing.Font("Arial", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRead.ForeColor = System.Drawing.Color.White;
+            this.btnRead.Location = new System.Drawing.Point(253, 641);
+            this.btnRead.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnRead.Name = "btnRead";
+            this.btnRead.Size = new System.Drawing.Size(220, 62);
+            this.btnRead.TabIndex = 6;
+            this.btnRead.Text = "Read";
+            this.btnRead.UseVisualStyleBackColor = false;
+            this.btnRead.Click += new System.EventHandler(this.btnRead_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.btnDelete.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnDelete.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDelete.Font = new System.Drawing.Font("Arial", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDelete.ForeColor = System.Drawing.Color.White;
+            this.btnDelete.Location = new System.Drawing.Point(27, 641);
+            this.btnDelete.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(220, 62);
+            this.btnDelete.TabIndex = 8;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = false;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnNew
+            // 
+            this.btnNew.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.btnNew.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnNew.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnNew.Font = new System.Drawing.Font("Arial", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnNew.ForeColor = System.Drawing.Color.White;
+            this.btnNew.Location = new System.Drawing.Point(851, 641);
+            this.btnNew.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnNew.Name = "btnNew";
+            this.btnNew.Size = new System.Drawing.Size(220, 62);
+            this.btnNew.TabIndex = 4;
+            this.btnNew.Text = "New";
+            this.btnNew.UseVisualStyleBackColor = false;
+            this.btnNew.Click += new System.EventHandler(this.btnNew_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.btnSave.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnSave.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSave.Font = new System.Drawing.Font("Arial", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSave.ForeColor = System.Drawing.Color.White;
+            this.btnSave.Location = new System.Drawing.Point(1076, 641);
+            this.btnSave.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(220, 62);
+            this.btnSave.TabIndex = 3;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = false;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // lbNotes
+            // 
+            this.lbNotes.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.lbNotes.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lbNotes.Font = new System.Drawing.Font("Arial", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbNotes.ForeColor = System.Drawing.Color.White;
+            this.lbNotes.HorizontalScrollbar = true;
+            this.lbNotes.ItemHeight = 32;
+            this.lbNotes.Location = new System.Drawing.Point(27, 91);
+            this.lbNotes.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.lbNotes.Name = "lbNotes";
+            this.lbNotes.Size = new System.Drawing.Size(446, 546);
+            this.lbNotes.TabIndex = 5;
+            this.lbNotes.TabStop = false;
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.btnEdit.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnEdit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnEdit.Font = new System.Drawing.Font("Arial", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEdit.ForeColor = System.Drawing.Color.White;
+            this.btnEdit.Location = new System.Drawing.Point(497, 641);
+            this.btnEdit.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(220, 62);
+            this.btnEdit.TabIndex = 7;
+            this.btnEdit.Text = "Edit";
+            this.btnEdit.UseVisualStyleBackColor = false;
+            this.btnEdit.Visible = false;
+            this.btnEdit.Click += new System.EventHandler(this.btnEdit_Click);
+            // 
+            // txtTitle
+            // 
+            this.txtTitle.BackColor = System.Drawing.Color.White;
+            this.txtTitle.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtTitle.Font = new System.Drawing.Font("Arial", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTitle.Location = new System.Drawing.Point(497, 139);
+            this.txtTitle.Margin = new System.Windows.Forms.Padding(4);
+            this.txtTitle.MaxLength = 50;
+            this.txtTitle.Name = "txtTitle";
+            this.txtTitle.Size = new System.Drawing.Size(798, 39);
+            this.txtTitle.TabIndex = 1;
+            // 
+            // txtNote
+            // 
+            this.txtNote.BackColor = System.Drawing.Color.White;
+            this.txtNote.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtNote.Font = new System.Drawing.Font("Arial", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNote.Location = new System.Drawing.Point(497, 218);
+            this.txtNote.Margin = new System.Windows.Forms.Padding(4);
+            this.txtNote.MaxLength = 20000;
+            this.txtNote.Multiline = true;
+            this.txtNote.Name = "txtNote";
+            this.txtNote.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtNote.Size = new System.Drawing.Size(798, 397);
+            this.txtNote.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.label1.Location = new System.Drawing.Point(495, 103);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 32);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Title";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial Rounded MT Bold", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.label2.Location = new System.Drawing.Point(495, 182);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 32);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Note";
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.label3.Font = new System.Drawing.Font("Arial Rounded MT Bold", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(27, 29);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(446, 62);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Saved Notes";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(497, 9);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(481, 91);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 12;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(1319, 718);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtNote);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnEdit);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtTitle);
+            this.Controls.Add(this.lbNotes);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnNew);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnRead);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Note App";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnRead;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnNew;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.ListBox lbNotes;
+        private System.Windows.Forms.Button btnEdit;
+        private System.Windows.Forms.TextBox txtTitle;
+        private System.Windows.Forms.TextBox txtNote;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Note Taking Application/Form1.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+namespace Note_Taking_Application
+{
+    public partial class Form1 : Form
+    {
+        DataTable notes = new DataTable();
+        OleDbConnection dbCon = new OleDbConnection(@"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=..\..\Database\Notes.accdb");
+        bool editFlag;
+        string prevTitle;
+        int indexOfPrevTitle;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            notes.Columns.Add("Title");
+            notes.Columns.Add("Note");
+
+            dbCon.Open();
+            OleDbCommand cmdRead = new OleDbCommand("Select Title from Notes", dbCon);
+            OleDbDataReader reader = cmdRead.ExecuteReader();
+            while (reader.Read())
+            {
+                lbNotes.Items.Add(reader["Title"].ToString());
+            }
+            dbCon.Close();
+        }
+
+        private void btnNew_Click(object sender, EventArgs e)
+        {
+            txtTitle.Text = "";
+            txtNote.Text = "";
+            txtTitle.Focus();
+            afterEdit();
+            lbNotes.SelectedItem = null;
+        }
+
+        void saveNew()
+        {
+            if (txtTitle.Text != "")
+            {
+                bool saveFlag = true;
+                for (int i = 0; i < lbNotes.Items.Count; i++)
+                {
+                    if (string.Equals(lbNotes.Items[i].ToString(), txtTitle.Text, StringComparison.CurrentCultureIgnoreCase))
+                    {
+                        saveFlag = false;
+                    }
+                }
+
+                if (saveFlag)
+                {
+                    dbCon.Open();
+                    OleDbCommand cmdInsert = new OleDbCommand("Insert Into Notes ([Title], [Note]) values('" + txtTitle.Text + "','" + txtNote.Text + "')", dbCon);
+                    cmdInsert.ExecuteNonQuery();
+                    lbNotes.Items.Add(txtTitle.Text.ToString());
+                    dbCon.Close();
+                    txtTitle.Text = "";
+                    txtNote.Text = "";
+                    txtTitle.Focus();
+                }
+                else
+                {
+                    MessageBox.Show("This Title is already in the list. Please give any different Title", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please give Title to the Note", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        void saveEdited()
+        {
+            try
+            {
+                dbCon.Open();
+                string query = "Update Notes set [Title] = '" + txtTitle.Text + "', [Note] = '" + txtNote.Text + "' where [Title] = '" + prevTitle + "'";
+                OleDbCommand cmdUpdate = new OleDbCommand(query, dbCon);
+                cmdUpdate.ExecuteNonQuery();
+                lbNotes.Items[indexOfPrevTitle] = txtTitle.Text;
+                txtTitle.Text = "";
+                txtNote.Text = "";
+                txtTitle.Focus();
+                dbCon.Close();
+            }
+            catch (Exception) { dbCon.Close(); }
+        }
+
+        void afterEdit()
+        {
+            prevTitle = "";
+            indexOfPrevTitle = -1;
+            editFlag = false;
+            btnEdit.Visible = false;
+            txtTitle.ReadOnly = false;
+            txtNote.ReadOnly = false;
+        }
+
+        private void btnEdit_Click(object sender, EventArgs e)
+        {
+            txtTitle.ReadOnly = false;
+            txtNote.ReadOnly = false;
+            prevTitle = txtTitle.Text;
+            indexOfPrevTitle = lbNotes.Items.IndexOf(prevTitle);
+            txtNote.Focus();
+            txtNote.Select(0, 0);
+            //txtNote.SelectionStart = txtNote.Text.Length;
+            //lbNotes.SelectedItem = null;
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            if (editFlag)
+            {
+                saveEdited();
+                afterEdit();
+            }
+            else
+            {
+                saveNew();
+                afterEdit();
+            }
+            lbNotes.SelectedItem = null;
+        }
+
+        private void btnRead_Click(object sender, EventArgs e)
+        {
+            dbCon.Open();
+            OleDbCommand cmdRead = new OleDbCommand("Select * from Notes where Title='"+lbNotes.Text+"'", dbCon);
+            OleDbDataReader reader = cmdRead.ExecuteReader();
+            if (reader.Read())
+            {
+                txtTitle.Text = reader["Title"].ToString();
+                txtNote.Text = reader["Note"].ToString();
+            }
+            dbCon.Close();
+            editFlag = true;
+            btnEdit.Visible = true;
+            btnEdit.Focus();
+            txtTitle.ReadOnly = true;
+            txtNote.ReadOnly = true;
+            //lbNotes.SelectedItem = null;
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                String listItem = lbNotes.Text;
+                dbCon.Open();
+                OleDbCommand cmdInsert = new OleDbCommand("Delete from Notes where Title= '" + listItem + "' ", dbCon);
+                cmdInsert.ExecuteNonQuery();
+                lbNotes.Items.Remove(listItem);
+                dbCon.Close();
+                txtTitle.Text = "";
+                txtNote.Text = "";
+                txtTitle.Focus();
+                afterEdit();
+            }
+            catch (Exception) { dbCon.Close(); }
+        }
+    }
+}
